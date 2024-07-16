e9eac8f2b5074211ed4eed32c7ca53e413d09212
Reichel-eze <reicheleze@gmail.com>
2024-07-16 15:19:06 -0300

    PARTE 2 HECHA, falta el ultimo punto

--- programa.pl
+++ programa.pl
@@ -114,3 +114,84 @@ parejaSoniada(Persona,Pareja) :-
     padre(Pareja, _),  % la pareja es padre 
     Persona \= Pareja,
     forall(padre(Persona, Hijo), padre(Pareja, Hijo)). % para todo hijo de una Persona, es hijo de la Pareja
+
+% SEGUNDA PARTE
+
+% 8) Agrego deportes que practican los deportistas y sus estadisticas
+% a) Natacion --> cant.metros, cant.medallas
+% b) Fulbol   --> cant.medallas, goles, expulsiones
+% c) Rugby    --> posicion, cant.medallas
+
+% practicaDeporte(deportista, deporte, cant.medallas)
+
+practicaDeporte(lisa, futbol(15, 100, 3)).
+practicaDeporte(rod, futbol(11, 0, 100)).
+
+practicaDeporte(lisa, natacion(1200, 20)).
+
+practicaDeporte(tod, rugby(pilar, 1)).
+practicaDeporte(rod, rugby(wings, 0)).
+
+% 9) Conocer a todas las personas que son nadadores
+nadador(Persona) :-
+    %persona(Persona),
+    practicaDeporte(Persona, natacion(_, _)).
+
+% 10) Conocer si una persona es buena deportista  --> si es buena en el deporte que practica
+
+esBuenaDeportista(Persona) :-
+    %practicaDeporte(Persona,_), % la persona practica algun deporte
+    esBuena(Persona).
+
+esBuenaDeportistaV2(Persona) :-
+    practicaDeporte(Persona, Deporte),
+    esBuenaV2(Deporte).
+
+% Una persona es buena en un deporte:
+% En el caso de la natación, si recorren más de 1.000 metros diarios.
+esBuena(Persona) :-
+    %persona(Persona),
+    practicaDeporte(Persona, natacion(Metros,_)),
+    Metros > 1000.
+
+% En el caso del fútbol, si la diferencia de goles menos las expulsiones suman más de 5
+esBuena(Persona) :-
+    %persona(Persona),
+    practicaDeporte(Persona, futbol(_, Goles, Expulsiones)),
+    Diferencia is Goles - Expulsiones,
+    Diferencia > 5.
+
+% En el caso del rugby, si son wings o pilares.
+esBuena(Persona) :-
+    %persona(Persona),
+    practicaDeporte(Persona, rugby(Posicion,_)),
+    member(Posicion, [pilar, wings]).
+
+% PASANDO EL DEPORTE COMO VARIABLE..
+
+esBuenaV2(natacion(Metros, _)) :- Metros > 1000.
+esBuenaV2(futbol(_, Goles, Expulsiones)) :- Diferencia is Goles - Expulsiones, Diferencia > 5.
+esBuenaV2(rugby(Posicion, _)) :- member(Posicion, [pilar, wings]).
+
+% 11) Una persona es exitosa en los deportes si en todos sus deportes tiene más de 10 medallas.
+esExitosa(Persona) :-
+    practicaDeporte(Persona,Deporte),
+    cuantasMedallas(Deporte,CantidadMedallas),
+    CantidadMedallas > 10.
+
+cuantasMedallas(natacion(_, CantidadMedallas), CantidadMedallas).
+cuantasMedallas(futbol(CantidadMedallas, _, _), CantidadMedallas).
+cuantasMedallas(rugby(_, CantidadMedallas), CantidadMedallas).
+
+% 12) Saber la cantidad total de medallas de una persona
+
+medallas(Persona, MedallasTotal) :-
+    practicaDeporte(Persona,Deporte),
+    cuantasMedallas(Deporte, MedallasTotal).
+
+% 13) Saber en cuantos deportes no es bueno.
+
+%cantidad(Persona, CantidadDeportes) :-
+%    practicaDeporte(Persona, Deporte),
+%    findall(Deporte, not(esBuenaDeportistaV2(Persona)), Deportes),
+%    length(Deportes, CantidadDeportes).
